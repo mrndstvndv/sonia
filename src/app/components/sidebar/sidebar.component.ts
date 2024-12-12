d169d3c64330641c0c5582500ef17d61634631a4
mrndstvndv <stvndvmrnd@gmail.com>
2024-12-12 22:40:44 +0800

    change sidebar placeholder to Sonia app icon

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,33 +1,37 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { IonButton, IonIcon } from '@ionic/angular/standalone';
+import { IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { compassOutline, folderOutline, homeOutline } from 'ionicons/icons';
+import { LucideAngularModule, Play } from 'lucide-angular';
 
 @Component({
-    selector: 'app-sidebar',
-    templateUrl: './sidebar.component.html',
-    standalone: true,
-    imports: [CommonModule, IonIcon]
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  standalone: true,
+  imports: [CommonModule, IonIcon, LucideAngularModule]
 })
 export class SidebarComponent {
-    @Input() isOpen: boolean = false;
-    @Output() toggleSidebar = new EventEmitter<void>();
+  @Input() isOpen: boolean = false;
+  @Output() toggleSidebar = new EventEmitter<void>();
 
-    constructor() { 
-        addIcons({
-            homeOutline,
-            compassOutline,
-            folderOutline
-        })
-    }
+  constructor() {
+    addIcons({
+      homeOutline,
+      compassOutline,
+      folderOutline
+    })
+  }
 
-    menuItems = [
-        { name: 'Home', icon: 'home-outline' },
-        { name: 'Playlists', icon: 'folder-outline' },
-    ];
 
-    onToggleSidebar() {
-        this.toggleSidebar.emit();
-    }
+  readonly Play = Play;
+
+  menuItems = [
+    { name: 'Home', icon: 'home-outline' },
+    { name: 'Playlists', icon: 'folder-outline' },
+  ];
+
+  onToggleSidebar() {
+    this.toggleSidebar.emit();
+  }
 }
